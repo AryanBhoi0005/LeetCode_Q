06d44ace7b59a4e66efd4ea771360d3b51c07a21
AryanBhoi0005 <159808093+AryanBhoi0005@users.noreply.github.com>
2024-08-21 08:27:52 +0530

    Time: 15 ms (25.03%), Space: 47.9 MB (5.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,17 @@
 class Solution {
+    static boolean PalinHelper(String s,int left,int right){
+      if(left>right){
+        return true;
+      }
+      else if(s.charAt(left)!=s.charAt(right)){
+        return false;
+      }
+      return PalinHelper(s,left+1,right-1);
+    
+    }
     public boolean isPalindrome(String s) {
         s=s.toLowerCase().replaceAll("[^a-z0-9]","");
         int n=s.length();
-        for(int i=0;i<s.length()/2;i++){
-            if(s.charAt(i)!=s.charAt(n-1-i)){
-                return false;
-            }
-        }
-        return true;
+        return PalinHelper(s,0,n-1);
     }
 }
