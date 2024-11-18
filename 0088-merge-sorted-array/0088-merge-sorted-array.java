0ad29ee7641f8ab3f2e854c97fb4fcbafcf38bb3
Aryan Bhoi <159808093+AryanBhoi0005@users.noreply.github.com>
2024-11-18 19:28:58 +0530

    Time: 1 ms (29.13%), Space: 42.2 MB (46.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,21 +5,13 @@ class Solution {
                 nums1[k]=nums2[k];
             }
         }
-        int idx=-1;
-        for(int i=n+m-1;i>=0;i--){
-            if(nums1[i]!=0){
-               idx=i+1;
-               break;
-            }
-        }
-        if(idx!=-1){
-           int i=0;
-            while(idx<nums1.length && i<nums2.length){
-                nums1[idx]=nums2[i];
-                i++;
-                idx++;
-            }
-        }
-        Arrays.sort(nums1);
+        int idx=m;
+        int i=0;
+        while(idx<n+m){
+            nums1[idx]=nums2[i];
+            idx++;
+            i++;
+    }
+    Arrays.sort(nums1);
     }
 }
